9d685e9bf4f94f1d38684699861b6fca2e44a0d9
Risma002 <59553437+Risma002@users.noreply.github.com>
2020-01-06 16:39:01 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+<html>
+<head>
+	<title>Cerpen_Islami</title>
+</head>
+
+<body bgcolor="#dacba5">
+
+<table border="0" background="bg10.png" style="background-size:100% 100%" border="0" width="100%" height="10%" align="center" cellpadding="0" cellspacing="0">
+	<tr height="1%" bgcolor="#9f957d"><td>
+		<nav>
+			<hr width="100%" align="center" noshade color="white">
+			<h1><font color="white">&nbsp;&nbsp;&nbsp;&nbsp;| Cerpen Islami |</font></h1>
+			<hr width="100%" align="center" noshade color="white">		
+		</nav>
+	</td></tr>
+</table>
+
+<hr width="100%" align="center" noshade color="white">
+
+<table border="0" width="90%" align="center" cellpadding="0" cellspacing="0">		
+		<tr>
+			<td align="justify" bgcolor="#33cc99">
+				<font size="25" face="segoe ui">
+					<table style="background-color:rgba(0, 0, 0, 0.5);" border="0" align="center" cellpadding="7"><tr><td><font color="#33cc99"><h2>Keutamaan Istighfar</h2></td></tr></table>
+					<table border="0" cellspacing="50" width="80%" align="center">
+					<tr>
+						<td>
+							<center><img src="pic.png" width="70%"></center>
+							<p align="justify">Tangis dan air mata seakan menjadi teman sehari-hari seorang janda bernama Aisyah. Bagaimana tidak, setelah menikah selama 15 tahun dan menjadi ibu rumah tangga seutuhnya ia kehilangan suami tercintanya.
+							<p align="justify">Meskipun alm suaminya merupakan karyawan tetap di sebuah perusahaan besar dan setelah meninggal ia mendapat cukup banyak pesangon, namun itu tidak akan cukup untuk membiayai kehidupannya dengan 4 orang anak dari pernikahannya dengan alm suaminya tersebut dalam kurun waktu yang lama.</p>
+							<p align="justify">Sebulan berlalu rasa rindu pada suaminya masih begitu terasa, dan peninggalan suaminya pun sudah semakin menipis. Sedangkan dirinya masih dalam masa indah yang membuatnya tak bisa bekerja keluar rumah.</p>
+							<p align="justify">Ya Aisyah adalah seorang wanita terjaga yang begitu rapat menutup diri dari perbuatan yang dilarang atau dimubahkan oleh agama. </p>
+							<p align="justify">Hingga tiba 40 hari ia ditinggal suaminya, pengajian pun dilaksanakan di kediamannya untuk mendoakan suaminya. Dalam pengajian tersebut ustadzah yang mengisi menceritakan tentang keutamaan istighfar.</p>
+							<p align="justify">“Barang siapa memperbanyak istighfar, segala yang ia minta akan dikabulkan oleh Allah dan rezeki mengalir akan didapatinya” sebuah kalimat yang begitu mengena di hati Aisyah.</p>
+							<p align="justify">Setelah pengajian selesai, Aisyah begitu memikirkan isi pengajian siang tadi. Akhirnya ia pun memutuskan untuk melakukan apa yang dikatakan oleh ustadzahnya.</p>
+							<p align="justify">Tak hanya dirinya, ia pun mengajak semua anaknya untuk mengalamkan dzikir ringan tersebut. Tiada hari, jam bahkan menit yang terlewat tanpa lafadz istighfar.</p>
+							<p align="justify">Hari demi hari berlalu, minggu demi minggu berlalu dan uang uang peninggalan suaminya semakin menipis sedangkan kebutuhan hidup semakin tinggi.</p>
+							<p align="justify">Ia bingung bukan main, sedangkan anak-anaknya masih kecil sehingga tak ada yang mengasuhnya jika ia pergi bekerja. Karena semakin bingung dan merasa tak ada orang yang mampu menolongnya, ia semakin rajin memanjatkan doa dan memperbanyak istighfar setiap harinya.</p>
+							<p align="justify">Keajaiban datang seketika itu juga.
+							<br>“Assalamualaikum, tok-tok” Terdengar suara di balik pintu ruang tamunya.
+							<br>Bergegas Aisyah pun membukakan pintu, ternyata seorang wanita paruh baya tak dikenal yang datang ke rumahnya.</p>
+							<p align="justify">Pembicaraan dimulai dengan hangat, hingga masuk pada pembicaraan inti. Wanita paruh baya tersebut ternyata seorang konglomerat dermawan yang sedang mencari lahan untuk membuat masjid.</p>
+							<p align="justify">Diketahui jika suami Aisyah juga meninggalkan sebuah pekarangan yang terletak di pinggir jalan dan cukup strategis. Lahan tersebut pun ditawar oleh wanita berparas cantik tersebut.</p>
+							<p align="justify">Karena tidak tahu pasaran harga tanah, Aisyah pun tidak mematok harga. Tanpa diduga, ternyata wanita tersebut memberi penawaran harga yang begitu tinggi.</p>
+							<p align="justify">“Bagaimana jika lahan ibu saya ambil alih dengan harga 1 Milyar?” ucapnya lembut.
+							<br>Dengan mata berkaca-kaca Aisyah sontak mengiyakan tawaran tersebut.
+							<br>“Boleh, sangat boleh. Suami saya pasti sangat bahagia jika lahan yang lama tak terpakai tersebut bisa bermanfaat bagi keluarganya dan dibuat untuk lahan masjid” jawabnya.
+							<br>“Baik setelah ini saya akan mengurus pemindah namaan sertifikat tanah dan pembayaran akan saya lakukan secara cash”.</p>
+							<p align="justify">Sore harinya, dua orang pria dengan pakaian serba hitam datang membawa koper ke rumahnya pagi tadi dan koper yang dibawanya berisikan uang senilai 1 milyar. ke rumah Aisyah. Ternyata dua orang tersebut adalah ajudan dari wanita yang datang</p>
+							<p align="justify">Penandatanganan penjualan pun ia lakukan untuk memproses pemindah namaan sertifikat tanah. Uang senilai 1 milyar pun ia dapatkan.</p>
+							<p align="justify">Tangis haru tak bisa ia bendung lagi. Bagaimana cara Allah mengeluarkan hamba-Nya dari kesulitan sungguh di luar perkiraan. Ada saja cara yang Allah pilihkan untuk memberikan kebahagiaan dan jalan keluar bagi hamba yang mau memohon.</p>
+							<p align="justify">Setelah hari itu, Aisyah dan anak-anaknya pun tak pernah henti mengucapkan istighfar setiap saat. Sedangkan uang yang ia dapat dari penjualan tanah peninggalan alm. suaminya ia gunakan untuk biaya sekolah anak-anak dan biaya membuka bisnis catering , Ya, ia lebih memilih bisnis rumahan karena dengannya ia tetap bisa memantau anak-anaknya di rumah.</p>
+							<hr width="50%" color="black" noshade>
+						
+						</td>
+					</tr>
+					</table>
+				</font>	
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<table border="0" width="100%" align="center" valign="center" cellpadding="10" cellspacing="0">
+					<tr bgcolor="#009b77" align="center">
+					<td><font face="Book Antiqua" color="white">copyright &copy;2020 | Website By. Risma | UM PAREPARE</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+
+<hr width="100%" align="center" noshade color="white">
+
+<br>
+
+	
+</body>
+</html>
